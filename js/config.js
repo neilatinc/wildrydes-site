1b9d3a6db0e2e6fa0ff1aa0beb21a6adf907dbf0
roxane <roxyl.rd@gmail.com>
2024-01-10 14:48:28 +0100

    new config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-1_lVbCgedA2', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2492g17tfvim0lr960es38sltr', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
